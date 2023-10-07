fef0b04f226afc5efac611ae233a4e6ab0a392ec
Mohamed Ashour <boxv38145@gmail.com>
2023-10-07 19:05:06 +0300

    optimized resposive

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -5,23 +5,23 @@ function Navbar() {
         <nav className="flex justify-between items-center px-6 text-white max-h-16 bg-black bg-opacity-60">
             <div className="flex">
                 <div className="cursor-pointer hover:text-gray-400 transition duration-100 ease-in-out">
-                    <svg className="sm:w-7 lg:w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
+                    <svg className="w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
                     </svg>
                 </div>
                 <div className="ml-3 cursor-pointer hover:text-gray-400 transition duration-100 ease-in-out">
-                    <svg className="sm:w-7 lg:w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px">
+                    <svg className="w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
                 </div>
                 <div className="ml-3 cursor-pointer hover:text-gray-400 transition duration-100 ease-in-out">
-                    <svg className="sm:w-7 lg:w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
+                    <svg className="w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                     </svg>
                 </div>
             </div>
             <div className="flex items-center xl:w-2/3 xl:text-sm justify-center 2xl:text-base lg:text-xs">
-                <div className="sm:hidden lg:block xl:block">
+                <div className="hidden sm:hidden lg:block xl:block">
                     <NavLink to="/games" className="mr-6 cursor-pointer hover:text-gray-400 transition duration-100 ease-in-out">GAMES</NavLink>
                     <NavLink to="/nfos" className="mr-6 cursor-pointer hover:text-gray-400 transition duration-100 ease-in-out">NFOS</NavLink>
                     <NavLink to="/groups" className="mr-6 cursor-pointer hover:text-gray-400 transition duration-100 ease-in-out">GROUPS</NavLink>
@@ -38,7 +38,7 @@ function Navbar() {
                         <div className="cursor-auto" style={{borderLeft:'65px solid transparent', borderRight:'65px solid transparent', borderTop:'36.6px solid rgba(0,0,0,.6)', position:'absolute', right:'50%', top:'85%', transform: 'translateX(50%)', zIndex:'-1'}}></div>
                     </NavLink>
                 </div>
-                <div className="sm:hidden lg:block xl:block">
+                <div className="hidden sm:hidden lg:block xl:block">
                     <NavLink to="/markets" className="cursor-pointer hover:text-gray-400 transition duration-100 ease-in-out">MARKETS</NavLink>
                     <NavLink to="/free-keys" className="ml-6 cursor-pointer hover:text-gray-400 transition duration-100 ease-in-out">FREE KEYS</NavLink>
                     <NavLink to="/get-karma" className="ml-6 cursor-pointer hover:text-gray-400 transition duration-100 ease-in-out">GET KARMA</NavLink>
@@ -48,19 +48,19 @@ function Navbar() {
             <div className="flex">
                 <NavLink to="/messages"  className="mr-3 hover:text-gray-400 transition duration-100 ease-in-out">
                     <div>
-                        <svg className="sm:w-7 lg:w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
+                        <svg className="w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                         </svg>
                     </div>
                 </NavLink>
                 <div className="mr-3 hover:text-gray-400 transition duration-100 ease-in-out">
-                    <svg className="sm:w-7 lg:w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
+                    <svg className="w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                     </svg>
                 </div>
                 <NavLink to="/login">
                     <div className="login-icon hover:text-gray-400 transition duration-100 ease-in-out">
-                        <svg className="sm:w-7 lg:w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
+                        <svg className="w-6 xl:w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
                     </div>
