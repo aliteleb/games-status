5392001ba96a55c4786c67d77ce5d9f6f7814789
aliteleb <aliteleb123456@gmail.com>
2023-10-07 19:53:56 +0300

    Navbar responsive

---
+++ src/components/icons/MenuIcon.jsx
@@ -0,0 +1,7 @@
+export default function MenuIcon(props){
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="40px" width="30px" {...props}>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+        </svg>
+    );
+}
