5d0203a659b7f65611eadccac5d927627f1f9d21
aliteleb <aliteleb123456@gmail.com>
2023-10-07 13:27:08 +0300

    Game card skeleton

--- src/components/layouts/GameCard.jsx
+++ src/components/layouts/GameCard.jsx
@@ -1,8 +1,21 @@
-function GameCard(props) {
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+function GameCard() {
     return (
-        <div>
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
+        <div className={'p-2  h-full'}>
+
+            <div className={'border flex flex-col  h-full'}>
+                <img src={'/src/assets/game-placeholder.jpg'}  alt={'game'}/>
+                <div className={'px-2 py-1 my-auto'}>
+                    <h1>
+                        <Skeleton height={'16px'} />
+                    </h1>
+                    <h2><Skeleton  width={'80%'} height={'12px'}/></h2>
+                </div>
+
+            </div>
+
         </div>
     );
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import GameCard from "../layouts/GameCard.jsx";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className={'container'}>
+
+        <div className={'flex flex-wrap justify-between'}>
+            <div className={'w-6/12'}>
+                <GameCard/>
+            </div>
+            <div className="w-6/12 grid grid-cols-2">
+                <GameCard/>
+                <GameCard/>
+                <GameCard/>
+                <GameCard/>
+            </div>
+
+        </div>
+
+
+    </div>
   )
 }
 
