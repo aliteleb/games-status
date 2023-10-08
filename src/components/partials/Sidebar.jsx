831c788efb51b6871b30b965c2ad79cd3c607045
Mohamed Ashour <boxv38145@gmail.com>
2023-10-08 14:32:31 +0300

    Collapse sidebar when any page clicked

--- src/components/partials/Sidebar.jsx
+++ src/components/partials/Sidebar.jsx
@@ -11,42 +11,55 @@ import ProtectionsIcon from '../icons/ProtectionsIcon'
 import ForumIcon from '../icons/ForumIcon'
 
 function Sidebar() {
+
+  function collapseSidebar(){
+    document.querySelector('#sidebar').style.left = '-16rem';
+  }
+
   return (
     <div id={'sidebar'} className='flex flex-col w-64 pl-5 h-screen pt-6 absolute top-0 -left-64 transition-all bg-custom-black z-10'>
-      <div onClick={()=>{ document.querySelector('#sidebar').style.left = '-16rem';}} className='cursor-pointer'>
+      <div onClick={collapseSidebar} className='cursor-pointer w-fit'>
           <Xmark className={'text-white'}/>
       </div>
-      <Link to="login" className='flex items-center mt-4 '>
+
+      <Link onClick={collapseSidebar} to="login" className='flex items-center mt-4 '>
           <UserIcon className={'text-red-800'}/>
-          <span className='ml-[10px] text-white'>LOGIN | SIGN UP</span>
+          <span className='ml-[10px] text-white hover:text-gray-400 transition duration-100 ease-in-out'>LOGIN | SIGN UP</span>
       </Link>
-      <Link className='flex items-center mt-4' to="/">
+
+      <Link onClick={collapseSidebar} className='flex items-center mt-4' to="/">
           <HomeIcon className={'text-red-800'}/>
-          <span className='ml-[10px] text-white'>HOME</span>
+          <span className='ml-[10px] text-white hover:text-gray-400 transition duration-100 ease-in-out'>HOME</span>
       </Link>
-      <Link className='flex items-center mt-4 text-white' to="/games">
+
+      <Link onClick={collapseSidebar} className='flex items-center mt-4 text-white' to="/games">
           <GamesIcon />
-          <span className='ml-[10px] text-white'>GAMES</span>
+          <span className='ml-[10px] text-white hover:text-gray-400 transition duration-100 ease-in-out'>GAMES</span>
       </Link>
-      <Link className='flex items-center mt-4 text-white' to="/free-keys">
+
+      <Link onClick={collapseSidebar} className='flex items-center mt-4 text-white' to="/free-keys">
           <FreeKeysIcon className={'text-red-800'}/>
-          <span className='ml-[10px] text-white'>FREE KEYS</span>
+          <span className='ml-[10px] text-white hover:text-gray-400 transition duration-100 ease-in-out'>FREE KEYS</span>
       </Link>
-      <Link className='flex items-center mt-4 text-white' to="/groups">
+
+      <Link onClick={collapseSidebar} className='flex items-center mt-4 text-white' to="/groups">
           <GroupsIcon className={'text-red-800'}/>
-          <span className='ml-[10px] text-white'>GROUPS</span>
+          <span className='ml-[10px] text-white hover:text-gray-400 transition duration-100 ease-in-out'>GROUPS</span>
       </Link>
-      <Link className='flex items-center mt-4 text-white' to="/markets">
+
+      <Link onClick={collapseSidebar} className='flex items-center mt-4 text-white' to="/markets">
           <MarketsIcon className={'text-red-800'}/>
-          <span className='ml-[10px] text-white'>MARKETS</span>
+          <span className='ml-[10px] text-white hover:text-gray-400 transition duration-100 ease-in-out'>MARKETS</span>
       </Link>
-      <Link className='flex items-center mt-4 text-white' to="/markets">
+
+      <Link onClick={collapseSidebar} className='flex items-center mt-4 text-white' to="/protections">
           <ProtectionsIcon/>
-          <span className='ml-[10px] text-white'>PROTECTIONS</span>
+          <span className='ml-[10px] text-white hover:text-gray-400 transition duration-100 ease-in-out'>PROTECTIONS</span>
       </Link>
-      <Link className='flex items-center mt-4 text-white' to="/markets">
+      
+      <Link onClick={collapseSidebar} className='flex items-center mt-4 text-white' to="/forum">
           <ForumIcon/>
-          <span className='ml-[10px] text-white'>FORUM</span>
+          <span className='ml-[10px] text-white hover:text-gray-400 transition duration-100 ease-in-out'>FORUM</span>
       </Link>
     </div>
   )
