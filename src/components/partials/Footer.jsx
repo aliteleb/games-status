db50b946d03366aa7e3e5fd926e51122c70cb038
aliteleb <aliteleb123456@gmail.com>
2023-10-10 14:09:23 +0300

    Footer clean

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -7,8 +7,8 @@ import Instagram from '../icons/Instagram'
 function Footer() {
   return (
     <footer className="bg-black bg-opacity-60 rounded-lg shadow mt-20">
-        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-4">
+            <div className="flex flex-wrap items-center justify-between gap-x-2">
                 <div className='flex mb-6 sm:mb-0 text-gray-400'>
                     <Link to="#">
                         <Facebook />
@@ -20,25 +20,29 @@ function Footer() {
                         <Instagram />
                     </Link>
                 </div>
-                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
+                <ul className="w-1/2 flex flex-col md:flex-row flex-wrap gap-1 md:gap-5 items-center justify-end mb-6 text-sm font-medium text-gray-500 sm:mb-0">
                     <li>
-                        <Link to="/about" className="mr-4 hover:underline md:mr-6 hover:text-gray-300 ">About</Link>
+                        <Link to="/about" className="hover:underline hover:text-gray-300 ">About</Link>
                     </li>
                     <li>
-                        <Link to="/contact" className="mr-4 hover:underline md:mr-6 hover:text-gray-300">Contact</Link>
+                        <Link to="/contact" className="hover:underline hover:text-gray-300">Contact</Link>
                     </li>
                     <li>
-                        <Link to="/terms-conditions" className="mr-4 hover:underline md:mr-6 hover:text-gray-300 duration-200">Terms & Conditions</Link>
+                        <Link to="/terms-conditions" className="hover:underline hover:text-gray-300 duration-200">Terms & Conditions</Link>
                     </li>
                     <li>
                         <Link to="/frequency-questions" className="hover:underline hover:text-gray-300">Frequency Questions</Link>
                     </li>
                 </ul>
             </div>
-            <hr className="my-6 border-gray-500 sm:mx-auto lg:my-8" />
-            <span className="block text-sm text-gray-500 sm:text-center">
-                © 2023 <Link to="https://www.facebook.com/aliteleb183" target='_blank' class="hover:underline hover:text-gray-300 duration-200">Ali Teleb</Link>
-                <Link to="https://www.facebook.com/mohamedashour774499/" target='_blank' class="hover:underline hover:text-gray-300 duration-200 ml-2">Mohamed Ashour</Link>. All Rights Reserved.
+            <hr className="my-2 border-gray-500 mx-auto" />
+            <span className="block text-sm text-gray-500 text-center">
+                <div>Crack-Watcher © 2023, All Rights Reserved.</div>
+                <br/>
+                <div className="font-bold">Developed By</div>
+                <Link to="https://www.facebook.com/aliteleb183" target='_blank' className="hover:underline hover:text-gray-300 duration-200">Ali Teleb</Link>
+                <span className="mx-1">&</span>
+                <Link to="https://www.facebook.com/mohamedashour774499/" target='_blank' className="hover:underline hover:text-gray-300 duration-200">Mohamed Ashour</Link>.
             </span>
         </div>
     </footer>
