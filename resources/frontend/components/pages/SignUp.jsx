bf3e2c134a099f3f772954f928fcb5d7522323b2
aliteleb <aliteleb123456@gmail.com>
2023-10-12 21:06:41 +0300

    Signup validation

--- resources/frontend/components/pages/SignUp.jsx
+++ resources/frontend/components/pages/SignUp.jsx
@@ -58,7 +58,7 @@ export default function SignUp() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        validateForm();
+        validateForm(formData);
     };
 
     return (
