b7bff947d838054169f7177c8764588d66a8f484
aliteleb <aliteleb123456@gmail.com>
2023-10-17 15:45:54 +0300

    Processing icon login & signup

--- resources/frontend/components/pages/SignUp.jsx
+++ resources/frontend/components/pages/SignUp.jsx
@@ -356,10 +356,21 @@ export default function SignUp({loading, setLoading}) {
 
                     </div>
                     <button onClick={handleSubmit}
-                            className={`cursor-pointer w-max mt-6 text-gray-200 bg-btn hover:bg-btn-hover transition  font-medium rounded-lg text-sm px-5 py-2.5 mb-2 ${loading ? 'disabled-button hover:bg-[#282c39]' : ''}`}
+                            className={`cursor-pointer w-max mt-6 text-gray-200 bg-btn hover:bg-btn-hover transition font-medium rounded-lg text-sm px-5 py-2.5 mb-2 ${loading ? 'disabled-button hover:bg-[#282c39]' : ''}`}
                             disabled={loading}
                     >
-                        {loading ? 'Signing Up...' : 'Sign Up'}
+
+                        {loading ?
+                            <>
+                                <svg className="animate-spin -ml-1 mr-3 h-5 w-5 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                    <path className="opacity-75" fill="currentColor"
+                                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                                Signing Up...
+                            </> : <>Sign Up</>
+                        }
+
                     </button>
                     <div className='mt-4 text-sm text-gray-400'>Already have an account?
                         <Link to="/login" className='mx-2 text-gray-200 hover:text-gray-300 transition'>Login</Link>
--- resources/frontend/components/partials/Sidebar.jsx
+++ resources/frontend/components/partials/Sidebar.jsx
@@ -33,7 +33,7 @@ function Sidebar() {
     }
 
     return (
-        <div id={'sidebar'} className='flex flex-col w-64 xl:w-72 px-3 h-screen pt-6 fixed top-0 -left-72 transition-all bg-custom-black z-10'>
+        <div id={'sidebar'} className='flex flex-col w-64 xl:w-72 px-6 h-screen pt-6 fixed top-0 -left-72 transition-all bg-custom-black z-10'>
             <div onClick={collapseSidebar} className='cursor-pointer w-fit mb-4'>
                 <Xmark className={'text-white'}/>
             </div>
