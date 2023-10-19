e1d3875984f9fcd4878ffdf6d28417c220f13566
aliteleb <aliteleb123456@gmail.com>
2023-10-19 22:55:27 +0300

    protections load more icon

--- resources/frontend/components/pages/Protections.jsx
+++ resources/frontend/components/pages/Protections.jsx
@@ -36,6 +36,11 @@ export default function Protections() {
             <div className='mt-3 grid sm:grid-cols-2 lg:grid-cols-3 w-full gap-6'>
                 {showprotections || placeholder}
             </div>
+            <div className="p-4">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" className="spinner_P7sC mx-auto" fill="#ddd">
+                    <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" className="spinner_P7sC"/>
+                </svg>
+            </div>
         </>
     )
 }
