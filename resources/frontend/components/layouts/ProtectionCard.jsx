369ccefee9354c474d557e5d5ffd45eb2a430493
Mohamed Ashour <boxv38145@gmail.com>
2023-10-19 17:29:38 +0300

    Last Game

--- resources/frontend/components/layouts/ProtectionCard.jsx
+++ resources/frontend/components/layouts/ProtectionCard.jsx
@@ -20,7 +20,7 @@ const ProtectionsCard = (props) => {
             <div className={`flex flex-col`} >
                 <h1 className='text-center bg-slate-600 py-3 rounded-t'><span className={`${is_animated}`}>{protection.name}</span></h1>
                 <h2 className='py-5 px-5 bg-slate-600/10'>Games <span className={`${is_animated}`}>(<b>{protection.games_count}</b>)</span></h2>
-                <h3 className='py-5 px-5 bg-slate-700/10'>Last Game: <span className={`${is_animated}`}>{protection.last_game?.name || "N/A"}</span></h3>
+                <h3 className='py-5 px-5 bg-slate-700/10 text-center'>Last Game: <div className={`${is_animated} text-center`}>{protection.last_game?.name || "N/A"}</div></h3>
             </div>
         </div>
     );
