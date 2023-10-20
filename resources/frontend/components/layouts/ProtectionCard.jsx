d93b82ac2016aa565d56021811232895177d6f82
Mohamed Ashour <boxv38145@gmail.com>
2023-10-21 01:29:03 +0300

    Create Protection Page

--- resources/frontend/components/layouts/ProtectionCard.jsx
+++ resources/frontend/components/layouts/ProtectionCard.jsx
@@ -12,10 +12,10 @@ const ProtectionsCard = (props) => {
     let is_animated = props.animate === true ? 'fade ' : '';
 
     return (
-        <Link to={`/protection/${protection?.slug}`} className={`relative group bg-[#16191e] rounded maxHeight ${protection?'shadow hover:shadow-lg hover:bg-black/60':''} overflow-hidden transition ${props.className || ''}`}>
+        <div  className={`relative group bg-[#16191e] rounded maxHeight ${protection?'shadow hover:shadow-lg hover:bg-black/60':''} overflow-hidden transition ${props.className || ''}`}>
             <div className={`flex flex-col`} >
                 {protection &&
-                <>
+                <Link to={`/protection/${protection?.slug}`}>
                     <BsShieldExclamation className='text-red-800 w-64 h-20 opacity-20 absolute top-[4rem] right-[-5rem]'/>
                     <div className='text-center bg-red-900 opacity-90 py-3 group-hover:bg-red-700'>
                         <span className={`${is_animated}`}>{protection.name}</span>
@@ -30,7 +30,7 @@ const ProtectionsCard = (props) => {
                         <span className="text-sm text-gray-300">Last Game</span>
                         <span className={`${is_animated} col-auto text-center font-bold px-2`}>{protection.last_game?.name}</span>
                     </div>
-                </>
+                </Link>
                 }
                 {!protection &&
                     <>
@@ -58,7 +58,7 @@ const ProtectionsCard = (props) => {
                 }
 
             </div>
-        </Link>
+        </div>
     );
 }
 
