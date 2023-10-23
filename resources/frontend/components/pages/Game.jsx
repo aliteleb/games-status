60f07d5a52a82adfeafac3046559b6a2cf70b9c2
Mohamed Ashour <boxv38145@gmail.com>
2023-10-23 19:00:46 +0300

    changes

--- resources/frontend/components/pages/Game.jsx
+++ resources/frontend/components/pages/Game.jsx
@@ -1,9 +1,33 @@
 import React, {useEffect} from 'react'
+import {useParams} from 'react-router-dom';
 import Comment from '../partials/Comment'
+import ApiClient from '../../services/ApiClient'
+import Skeleton from 'react-loading-skeleton';
 
 function Game() {
 
+    const {slug} = useParams(null)
+
     let [count, setCount] = React.useState(0)
+    let [game, setGame] = React.useState(
+        {
+            status_text: null,
+            days_diff: null,
+            image: null,
+            is_following: null,
+            status_long: null,
+            status_color: null,
+            poster: null,
+            cover: null,
+            name: null,
+            release_date: null,
+            protections: [],
+            crack_date: null,
+            scene_group: null,
+            groups: [],
+            followers_count: null
+        }
+    )
 
     let increaseCount = ()=> {
         setCount(count = count + 1)
@@ -34,62 +58,90 @@ function Game() {
             lastComment.classList.remove('border-b-2')
         }
 
+        ApiClient().get(`/game/${slug}`)
+        .then(res => setGame(res.data.data))
+        .catch(err => console.log('Failed to get data', err))
+
     }, []);
 
-    let comments = document.getElementById('comments')
-    console.log(comments);
 
+    let comments = document.getElementById('comments')
+
+    game.status_color = game.status_text ? game.status_text.toLowerCase() : 'gray-600'
+    let icon, days
+    const statusText = game.status_text || ''
+
+    switch (statusText) {
+        case "CRACKED":
+            game.status_long = `AFTER ${game.days_diff} DAYS`
+            break
+        case "UNCRACKED":
+            game.status_long = `${game.days_diff} DAYS AND COUNTING`
+            break
+        default:
+            ""
+    }
+
+    days = `D${statusText === "CRACKED" || statusText === "UNCRACKED" ? "+" : "-"}${game.days_diff}`;
+
+    // let customStyles = props.animate === true ? 'fade ' : '';
+
+    console.log(game.drm_protection);
     return (
         <>
             <div id="blurred-bg" className="w-full h-screen absolute top-0 left-0 overflow-hidden">
-                <img className="opacity-30 top-0 w-full h-screen" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
-                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
-                <img className="opacity-30 top-0 w-full h-screen" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
-                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
-                <img className="opacity-30 top-0 w-full h-screen" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
-                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
-                <img className="opacity-30 top-0 w-full h-screen" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
-                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
-                <img className="opacity-30 top-0 w-full h-screen" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
-                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt="bg"/>
+                <img className="opacity-30 top-0 w-full h-screen" src={`${game.poster}`} alt="bg"/>
+                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src={`${game.poster}`} alt="bg"/>
+                <img className="opacity-30 top-0 w-full h-screen" src={`${game.poster}`} alt="bg"/>
+                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src={`${game.poster}`} alt="bg"/>
+                <img className="opacity-30 top-0 w-full h-screen" src={`${game.poster}`} alt="bg"/>
+                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src={`${game.poster}`} alt="bg"/>
+                <img className="opacity-30 top-0 w-full h-screen" src={`${game.poster}`} alt="bg"/>
+                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src={`${game.poster}`} alt="bg"/>
+                <img className="opacity-30 top-0 w-full h-screen" src={`${game.poster}`} alt="bg"/>
+                <img className="opacity-30 top-[100%] w-full h-screen -scale-y-100" src={`${game.poster}`} alt="bg"/>
 
                 <div className="absolute top-[4rem] w-full h-[200%] -translate-x-1/2 left-1/2 backdrop-blur-xl z-0"/>
             </div>
 
-            <div className='flex relative z-20 text-gray-300 border-t-[5px] border-uncracked h-[22rem] shadow-lg overflow-hidden'>
-                <img className='absolute w-full h-full z-[-1] object-cover opacity-70' src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_hero.jpg"
+            <div className={`flex relative z-20 text-gray-300  border-t-[5px] border-${game.status_color} h-[22rem] shadow-lg overflow-hidden`}>
+                <img className={`absolute w-full h-full z-[-1] object-cover opacity-70`} src={`${game.cover}`}
                      style={{aspectRatio: '1920/620'}}
                      alt=""/>
-                <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/library_600x900.jpg" alt=""/>
+                <img src={`${game.poster}`} alt=""/>
                 <div className='flex w-full items-center justify-between bg-black/80 py-3 px-5'>
                     <div>
                         <div>
                             <div className='flex justify-between'>
                                 <div className='text-lg text-white/40 font-extralight mr-2'>STATUS</div>
-                                <div className='text-lg text-uncracked'>316 DAYS AND COUNTING</div>
+                                <div className={`text-lg text-${game.status_color}`}>
+                                    {game.status_long || <Skeleton width={'100%'} height={'14px'} baseColor={'#27282e'} highlightColor={'#424349'} borderRadius={0}/>}
+                                </div>
                             </div>
-                            <div className='text-[3.5rem] rounded text-uncracked font-bold text-center -mt-5'>UNCRACKED</div>
+                            <div className={`text-${game.status_color} text-[3.5rem] rounded  font-bold text-center -mt-5`}>
+                                {game.status_text || <Skeleton width={'100%'} height={'40px'} baseColor={'#27282e99'} highlightColor={'#424349'} borderRadius={20}/>}
+                            </div>
                         </div>
                         <div className='w-full my-5'>
                             <div className='text-[#dddddd99] font-extralight'>GAME</div>
-                            <div className='text-gray-100 text-xl'>RED DEAD REDEMPTION 2</div>
+                            <div className='text-gray-100 text-xl'>{game.name}</div>
                         </div>
                         <div className='grid grid-cols-2 gap-y-4 justify-between'>
                             <div>
                                 <div className='text-[#dddddd99] font-extralight'>RELEASE DATE</div>
-                                <div className='text-xl text-gray-100'>NOV 05, 2019</div>
+                                <div className='text-xl text-gray-100'>{game.release_date}</div>
                             </div>
                             <div>
                                 <div className='text-[#dddddd99] font-extralight'>DRM PROTECTION</div>
-                                <div className='text-xl text-gray-100'>ROCKSTAR</div>
+                                <div className='text-xl text-gray-100'>{game.protections.map(drm => drm.name)}</div>
                             </div>
                             <div>
                                 <div className='text-[#dddddd99] font-extralight'>CRACK DATE</div>
-                                <div className='text-xl'>TBD</div>
+                                <div className='text-xl'>{game.crack_date}</div>
                             </div>
                             <div>
                                 <div className='text-[#dddddd99] font-extralight'>SCENE GROUP</div>
-                                <div className='text-xl'>TBD</div>
+                                <div className='text-xl'>{game.groups.map(group => group.name)}</div>
                             </div>
                         </div>
                     </div>
@@ -99,7 +151,7 @@ function Game() {
                     <div className='text-center'>AD3</div>
                     <div className='text-center'>
                         <div className='text-3xl'>FOLLOWERS</div>
-                        <div className='my-2 text-2xl'>105,297</div>
+                        <div className='my-2 text-2xl'>{game.followers_count}</div>
                         <div className="flex flex-wrap justify-center py-2 rounded ">
                             <input
                                 type='checkbox'
