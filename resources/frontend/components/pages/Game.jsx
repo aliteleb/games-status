0733fc80e1a8ec8d2c697a187c7103444f61ee4a
Mohamed Ashour <boxv38145@gmail.com>
2023-10-23 22:04:23 +0300

    Button hover

--- resources/frontend/components/pages/Game.jsx
+++ resources/frontend/components/pages/Game.jsx
@@ -200,7 +200,7 @@ function Game() {
                                     defaultValue={""}
                                 />
                             </div>
-                            <button type="button" class="transition text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+                            <button type="button" class="transition text-gray-300 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                                 Post comment
                             </button>
 
