5b750f7e0a21d566ba6be7f82d7c9140d1c56b0e
aliteleb <aliteleb123456@gmail.com>
2023-10-24 18:19:27 +0300

    Comments and Reactions database

---
+++ app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'game_id', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function game()
+    {
+        return $this->belongsTo(Game::class, 'game_id');
+    }
+    public function reactions()
+    {
+        return $this->belongsTo(Reaction::class, 'comment_id');
+    }
+}
