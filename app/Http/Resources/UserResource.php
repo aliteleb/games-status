42b9dc372d8d213e81302f23c8ce03dba7b5ad4f
aliteleb <aliteleb123456@gmail.com>
2023-10-28 01:54:48 +0200

    User avatar

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,6 +18,8 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'username' => $this->username,
             'email' => $this->email,
+            'avatar' => $this->avatar,
+            'small_avatar' => $this->small_avatar,
             // 'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             // 'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
