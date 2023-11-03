d1d68fcbb2abd366028947d34ace833688e86c8c
aliteleb <aliteleb123456@gmail.com>
2023-11-04 01:07:37 +0200

    Notifications backend

--- app/Http/Resources/GameResource.php
+++ app/Http/Resources/GameResource.php
@@ -44,6 +44,9 @@ class GameResource extends JsonResource
             'status_text' => $statusText,
             'days_diff' => $daysDifference,
             'image' => 'https://cdn.cloudflare.steamstatic.com/steam/apps/'.$this->steam_appid.'/header.jpg',
+            'header' => 'https://cdn.cloudflare.steamstatic.com/steam/apps/' . $this->steam_appid . '/header.jpg',
+            'poster' => 'https://cdn.cloudflare.steamstatic.com/steam/apps/' . $this->steam_appid . '/library_600x900.jpg',
+            'cover' => 'https://cdn.cloudflare.steamstatic.com/steam/apps/' . $this->steam_appid . '/library_hero.jpg',
             'is_following' => $is_following
             // 'protections' => ProtectionResource::collection($this->whenLoaded('protections')),
             // 'groups' => GroupResource::collection($this->whenLoaded('groups')),
