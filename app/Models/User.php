536bef4893746da2e25354e2ad01f3e66ad17bb7
aliteleb <aliteleb123456@gmail.com>
2023-11-04 00:16:38 +0200

    Notifications backend

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,7 +61,7 @@ class User extends Authenticatable
     }
     public function notifications()
     {
-        return $this->hasMany(Notification::class)->with('user');
+        return $this->hasMany(Notification::class)->with('comment');
     }
 
     public function getFollowingAttribute()
