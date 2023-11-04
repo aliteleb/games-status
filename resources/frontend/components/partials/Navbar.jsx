3f7fbd0f4f6e0a978ba8353da84c0ae65b85aa46
Mohamed Ashour <boxv38145@gmail.com>
2023-11-04 21:12:52 +0200

    Notifications

--- resources/frontend/components/partials/Navbar.jsx
+++ resources/frontend/components/partials/Navbar.jsx
@@ -53,19 +53,19 @@ function Navbar() {
 
     return (
         <>
-            <nav className="fixed z-50 grid w-full items-center px-1 text-white backdrop-blur-xl grid-cols-[1fr_auto_1fr] bg-app-black/70 md:px-6">
+            <nav className="fixed z-50 grid w-full grid-cols-[1fr_auto_1fr] items-center bg-app-black/70 px-1 text-white backdrop-blur-xl md:px-6">
                 <div className="flex items-center" id="left-nav">
                     <RxHamburgerMenu onClick={() => {
                         document.getElementById('sidebar').style.left = '0';
                         document.getElementById('left-nav').style.opacity = '0';
-                    }} className={'cursor-pointer hover:text-gray-400 transition w-6 h-6'}/>
+                    }} className={'h-6 w-6 cursor-pointer transition hover:text-gray-400'}/>
                     <Link to="/games">
-                        <BsSearch className={'mx-4 w-6 h-6 cursor-pointer hover:text-gray-400 transition'}/>
+                        <BsSearch className={'mx-4 h-6 w-6 cursor-pointer transition hover:text-gray-400'}/>
                     </Link>
 
                 </div>
 
-                <div className="grid items-center justify-center grid-cols-[auto_1fr_auto] h-[4rem] sm:grid-cols-[1fr_auto_1fr]">
+                <div className="grid h-[4rem] grid-cols-[auto_1fr_auto] items-center justify-center sm:grid-cols-[1fr_auto_1fr]">
                     <div className="hidden text-end sm:block">
                         <NavLink onClick={collapseSidebar} to="/games" className="mx-2 hidden cursor-pointer transition hover:text-gray-400 xl:inline-block">GAMES</NavLink>
                         <NavLink onClick={collapseSidebar} to="/groups" className="mx-2 hidden cursor-pointer transition hover:text-gray-400 lg:inline-block">GROUPS</NavLink>
@@ -73,8 +73,8 @@ function Navbar() {
                     </div>
                     <div className="mx-3 md:mx-6">
                         <NavLink onClick={collapseSidebar} to="/" className="relative">
-                            <img src="/assets/images/logo.png" className="relative top-5 h-16 w-16 animate-glow" alt="Logo"/>
-                            <div className="absolute cursor-auto w-[10rem] h-[10rem] bg-app-black/70" style={{
+                            <img src="/assets/images/logo.png" className="animate-glow relative top-5 h-16 w-16" alt="Logo"/>
+                            <div className="absolute h-[10rem] w-[10rem] cursor-auto bg-app-black/70" style={{
                                 top: '-100%',
                                 left: '-75%',
                                 zIndex: '-1',
@@ -83,9 +83,9 @@ function Navbar() {
                         </NavLink>
                     </div>
                     <div className="hidden sm:block">
-                        <span onClick={collapseSidebar} className="mx-2 cursor-not-allowed transition hover:text-gray-400 font-bold">FREE GAMES</span>
-                        <span onClick={collapseSidebar} className="mx-2 hidden cursor-not-allowed transition hover:text-gray-400 lg:inline-block font-bold">POINTS</span>
-                        <span onClick={collapseSidebar} className="mx-2 hidden cursor-not-allowed hover:text-gray-400 xl:inline-block font-bold">MARKET</span>
+                        <span onClick={collapseSidebar} className="mx-2 cursor-not-allowed font-bold transition hover:text-gray-400">FREE GAMES</span>
+                        <span onClick={collapseSidebar} className="mx-2 hidden cursor-not-allowed font-bold transition hover:text-gray-400 lg:inline-block">POINTS</span>
+                        <span onClick={collapseSidebar} className="mx-2 hidden cursor-not-allowed font-bold hover:text-gray-400 xl:inline-block">MARKET</span>
                     </div>
                 </div>
 
@@ -96,14 +96,14 @@ function Navbar() {
                             <RiNotification2Line onClick={() => {
                                 setShowNotificationPopup(!showNotificationPopup)
                             }} id="notification-dropdown"
-                                 className={`mx-4 transition w-6 h-6 rounded-full cursor-pointer ${showNotificationPopup ? 'text-gray-200 hover:text-gray-200' : 'text-gray-300 hover:text-gray-400'}`}
+                                 className={`border-2 hover:border-gray-400 rounded-full mx-4 transition p-2 w-10 h-10 rounded-full cursor-pointer ${showNotificationPopup ? 'border-gray-400' : 'border-gray-600'}`}
                                  src={`${user.avatar}`} alt={'avatar'} width={100} height={100}/>
-                            {unReadNotificationsCount === 0 || <div className="absolute top-[-10px] text-xs right-8 bg-red-800 rounded-full flex items-center justify-center w-5 h-5">{unReadNotificationsCount}</div>}
+                            {unReadNotificationsCount === 0 || <div className="absolute right-8 top-[-10px] flex h-5 w-5 items-center justify-center rounded-full bg-red-800 text-xs">{unReadNotificationsCount}</div>}
                         </div>
                     }
                     {!user &&
                         <Link to="/login">
-                            <RiNotification2Line className="mx-2 w-6 h-6 text-gray-300 transition hover:text-gray-400"/>
+                            <RiNotification2Line className="mx-2 h-6 w-6 text-gray-300 transition hover:text-gray-400"/>
                         </Link>
                     }
 
@@ -116,20 +116,20 @@ function Navbar() {
                     }
                     {!user &&
                         <Link to="/login">
-                            <BiUserCircle className="mx-2 w-8 h-8 text-gray-300 transition hover:text-gray-400"/>
+                            <BiUserCircle className="mx-2 h-8 w-8 text-gray-300 transition hover:text-gray-400"/>
                         </Link>
                     }
                 </div>
             </nav>
             <div className={`${!showProfilePopup ? 'hidden' : 'animate-slide-down'} avatar-popup z-50 w-48 my-4 text-base list-none rounded-lg shadow bg-black/60 fixed right-[.7rem] top-[3.2rem]`}
                 id="user-dropdown">
-                <div className="px-4 py-3 hover:bg-neutral-700/30 rounded-t-lg">
+                <div className="rounded-t-lg px-4 py-3 hover:bg-neutral-700/30">
                     <Link onClick={() => setShowProfilePopup(false)} to={`/user/${user?.username}`}>
                         <span className="block text-center text-sm text-gray-400">
                             @{user?.username}
                         </span>
                         <span className="mt-1 block truncate text-center text-sm text-gray-400">
-                            <HiOutlineMail className="mx-1 inline h-5 w-5 text-gray-400 "/>
+                            <HiOutlineMail className="mx-1 inline h-5 w-5 text-gray-400"/>
                             {user?.email}
                         </span>
                     </Link>
@@ -144,7 +144,7 @@ function Navbar() {
                     </li>
                     <li>
                         <NavLink onClick={() => setShowProfilePopup(false)} to="/logout"
-                                 className="block py-2 text-sm text-gray-400 hover:bg-neutral-700/30 rounded-b-lg">
+                                 className="block rounded-b-lg py-2 text-sm text-gray-400 hover:bg-neutral-700/30">
                                  <GoSignOut className="mx-2 inline h-5 w-5 text-gray-400"/>
                                  Sign out
                         </NavLink>
@@ -153,11 +153,11 @@ function Navbar() {
             </div>
             <div className={`${!showNotificationPopup ? 'hidden' : 'animate-slide-down'} border border-red-800 rounded-tl-none rounded-tr-none notification-popup shadow-black flex rounded-md overflow-hidden flex-col z-50 w-full md:w-[35rem] mt-12 md:my-4 md:mx-[4.60rem] text-gray-300 list-none shadow bg-[#1d1c1c] fixed md:right-[0.7rem] top-[3.2rem]`}
                  id="notification-dropdown">
-                <div className="flex justify-center items-center gap-x-3 bg-red-700 h-10 ">
+                <div className="flex h-10 items-center justify-center gap-x-3 bg-red-700">
                     Notifications
-                    <div className="bg-red-950 rounded-full px-3 py-[2px] flex justify-center items-center">{notificationsCount}</div>
+                    <div className="flex items-center justify-center rounded-full bg-red-950 px-3 py-[2px]">{notificationsCount}</div>
                 </div>
-                <div className="p-2 bg-black/50 flex flex-col gap-y-2">
+                <div className="flex flex-col gap-y-2 bg-black/50 p-2">
                     <Notifications setUnReadNotificationsCount={setUnReadNotificationsCount} setNotificationsCount={setNotificationsCount}/>
                 </div>
 
