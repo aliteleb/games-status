a03672502bfd8223cdcb0378c5ed65ad381395d0
aliteleb <aliteleb123456@gmail.com>
2023-11-04 23:07:48 +0200

    Notifications backend

--- resources/frontend/components/layouts/Notifications.jsx
+++ resources/frontend/components/layouts/Notifications.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
-import GameNotification from '../Notifications/GameNotification'
-import ReplyNotification from '../Notifications/ReplyNotifications'
-import ApiClient from '../../services/ApiClient'
+import React from "react";
+import GameNotification from "../Notifications/GameNotification";
+import ReplyNotification from "../Notifications/ReplyNotifications";
+import ApiClient from "../../services/ApiClient";
 
 function Notifications(props) {
 
-  let [response, setResponse] = React.useState(null)
-  let [notifications, setNotifications] = React.useState(null)
+    let [response, setResponse] = React.useState(null);
+    let [notifications, setNotifications] = React.useState(null);
 
-  React.useEffect( () => {
+    React.useEffect(() => {
 
-    setNotifications(window.appData.notifications.notifications)
-    props.setNotificationsCount(window.appData.notifications.notifications_count)
-    props.setUnReadNotificationsCount(window.appData.notifications.unread_notifications)
+        setNotifications(window.appData.notifications.notifications);
+        props.setNotificationsCount(window.appData.notifications.notifications_count);
+        props.setUnReadNotificationsCount(window.appData.notifications.unread_notifications);
 
-    const interval = setInterval(() => {
-      ApiClient().get('/notifications')
-      .then(res => {
-        setResponse(res.data.data)
-        setNotifications(res.data.data.notifications)
-        props.setNotificationsCount(res.data.data.notifications_count)
-        props.setUnReadNotificationsCount(res.data.data.unread_notifications)
-      })
-      .catch(err => console.log(err))
+        const interval = setInterval(() => {
+            ApiClient().get("/notifications")
+                .then(res => {
+                    setResponse(res.data.data);
+                    setNotifications(res.data.data.notifications);
+                    props.setNotificationsCount(res.data.data.notifications_count);
+                    props.setUnReadNotificationsCount(res.data.data.unread_notifications);
+                })
+                .catch(err => console.log(err));
 
-    }, 5000)
-  }, [])
+        }, 5000);
+    }, []);
 
-
-  let replyNotifications = notifications?.map((item, index) => item.type === 'reply' ? <ReplyNotification setNotifications={setNotifications} setUnReadNotificationsCount={props.setUnReadNotificationsCount} key={index} info={item}/> : <GameNotification setNotifications={setNotifications} setUnReadNotificationsCount={props.setUnReadNotificationsCount} key={index} info={item}/>)
-
-  return (
-    <>
-      {replyNotifications}
-    </>
-  )
+    return (
+        <>
+            {
+                notifications?.map((item, index) => item.type === "reply" ?
+                    <ReplyNotification setNotifications={setNotifications} setUnReadNotificationsCount={props.setUnReadNotificationsCount} key={index} info={item} /> :
+                    <GameNotification setNotifications={setNotifications} setUnReadNotificationsCount={props.setUnReadNotificationsCount} key={index} info={item} />)
+            }
+        </>
+    );
 }
 
-export default Notifications
+export default Notifications;
