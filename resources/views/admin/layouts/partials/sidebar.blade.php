957c63abb82b608ab0dd1745a706ee737328b3c9
aliteleb <aliteleb123456@gmail.com>
2023-11-07 00:19:55 +0200

    game dashboard

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -36,7 +36,6 @@
             </div>
         </div>
 
-
         <div class="sidebar-category sidebar-category-visible">
             <div class="category-content no-padding">
                 <ul class="navigation navigation-main navigation-accordion">
