70ec5132cac6acff0d53f165548f80e9503cd7a5
aliteleb <aliteleb123456@gmail.com>
2023-11-22 23:54:28 +0200

    public redirect

--- resources/frontend/index.jsx
+++ resources/frontend/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from "./components/App.jsx";
 import {AuthProvider} from "./components/api/AuthContext.jsx";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
